7112324fb34f1cb6e6d40f6b0bc6f78a111c65fa
Abhimanyu kumar <9905abhimanyu@gmail.com>
2024-04-14 11:12:23 +0530

    updated search.js

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,21 +3,17 @@ const router = express.Router();
 const axios = require("axios");
 require("dotenv").config();
 
+const baseUrl = "https://api.amadeus.com/v1/reference-data/locations/pois";
+
 router.get("/city", async (req, res) => {
   try {
     const accessToken = await getAccessToken();
-    const response = await axios.get(
-      "https://api.amadeus.com/v1/reference-data/locations/pois",
-      {
-        params: {
-          keyword: req.query.city,
-          categories: "SIGHTS",
-        },
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const cityName = req.query.city;
+    const response = await axios.get(`${baseUrl}?cityCode=${cityName}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     console.log("Response:", response.data);
     res.status(200).json(response.data);
   } catch (error) {
@@ -26,25 +22,5 @@ router.get("/city", async (req, res) => {
   }
 });
 
-async function getAccessToken() {
-  try {
-    const response = await axios.post(
-      "https://test.api.amadeus.com/v1/security/oauth2/token",
-      {
-        grant_type: "client_credentials",
-        client_id: process.env.AMADEUS_API_KEY,
-        client_secret: process.env.AMADEUS_API_SECRET,
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
 
-    return response.data.access_token;
-  } catch (error) {
-    console.error("Error getting access token:", error);
-  }
-}
 module.exports = router;
