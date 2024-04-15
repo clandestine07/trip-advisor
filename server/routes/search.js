fa6f3146f84f36bc1c4df16aa929f8307fbc3ca4
Abhimanyu kumar <95843474+clandestine07@users.noreply.github.com>
2024-04-15 10:45:44 +0530

    Updated search.js

--- server/routes/search.js
+++ server/routes/search.js
@@ -21,6 +21,24 @@ router.get("/city", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+async function getAccessToken() {
+  try {
+    const response = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', {
+      grant_type: 'client_credentials',
+      client_id: process.env.AMADEUS_API_KEY,
+      client_secret: process.env.AMADEUS_API_SECRET
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 
+    console.log('Access token response:', response.data);
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error getting access token:', error.response.data);
+    throw error;
+  }
+}
 
 module.exports = router;
